590a10860dfb0bc8575556b57c4a125a3cc380c9
Szn <julien.sezn@gmail.com>
2022-08-17 19:54:59 +0200

    README

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 #Test API Kaamelott w Jetpack Compose
 
-API: https://kaamelott.chaudie.re/api
-Doc: https://github.com/sin0light/api-kaamelott
+##API
+https://kaamelott.chaudie.re/api
+
+##Doc 
+https://github.com/sin0light/api-kaamelott
 
 
-## Libs:
-Hilt
-Jetpack compose
-Retrofit & Okhttp
+##Libs:
+   Hilt
+   Jetpack compose
+   Retrofit & Okhttp
--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,14 +3,6 @@
  * and its source artifacts are available throughout your entire build script
  */
 object Versions {
-    val versionName = "7.0.15"
-    private val versionCodeBase = 70150 // XYYZZM; M = Module (tv, mobile)
-    val versionCodeMobile = versionCodeBase + 3
-
-    const val COMPILE_SDK = 31
-    const val TARGET_SDK = 30
-    const val MIN_SDK = 21
-
     const val HILT = "2.43.2"
     const val COMPOSE = "1.2.1"
     const val COROUTINES = "1.6.3"
